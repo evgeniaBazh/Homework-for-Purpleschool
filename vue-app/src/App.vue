99b31faaf297b83ea616fa47afc6340277c45997
evgeniaBazh <evgeniabazh7@gmail.com>
2025-09-22 13:09:18 +0500

    Инициализация проекта

---
+++ vue-app/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    timezone: 'UTC'
+};
+  const now = new Date().toLocaleString("ru", options); 
+</script>
+
+<template>
+  <div class="wrap">
+    <h1 class="date">{{ now }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  display: flex;
+  justify-content: center;
+}
+.date {
+  border: 1px solid green;
+  padding: 5px 15px;
+  width: max-content;;
+}
+</style>
