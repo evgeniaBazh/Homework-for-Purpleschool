dd6f760547d5a0c39eabdcaafe98c5f22a97f4cb
evgeniaBazh <evgeniabazh7@gmail.com>
2025-10-27 13:02:03 +0500

    Вертска карточек и их вывод

---
+++ card-app/src/assets/Cross.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    
+<svg width="39" height="40" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M26.422 24.278L24.3 26.4L19.498 21.602L14.698 26.394L12.578 24.272L17.376 19.482L12.578 14.686L14.7 12.564L19.5 17.362L24.302 12.568L26.422 14.692L21.622 19.482L26.422 24.278ZM19.5 0C8.748 0 0 8.748 0 19.5C0 30.252 8.748 39 19.5 39C30.252 39 39 30.252 39 19.5C39 8.748 30.252 0 19.5 0Z" fill="#D00303"/>
+</svg>
+
+
+</template>
+
+<style scoped></style>
--- card-app/src/components/Card.vue
+++ card-app/src/components/Card.vue
@@ -1,12 +1,34 @@
 <script setup>
 import { ref } from 'vue'
+import Cross from '../assets/Cross.vue'
+import CrossMini from '../assets/CrossMini.vue'
+import CheckMark from '../assets/CheckMark.vue'
+import CheckMarkMini from '../assets/CheckMarkMini.vue'
 const emit = defineEmits(['flip', 'cardStatus'])
-const card = ref({
-    word: 'hello',
-    translation: 'привет',
+const cards = ref([{
+    word: 'dust-coat',
+    translation: 'пятно на пальто',
     state: 'closed',
     status: 'pending',
-})
+    },
+    {
+    word: 'camel caravan',
+    translation: 'караван верблюдов',
+    state: 'open',
+    status: 'pending',
+    },
+    {word: 'auto',
+    translation: 'автомобиль',
+    state: 'open',
+    status: 'succes',
+    },
+    {word: 'lead',
+    translation: 'свинец',
+    state: 'open',
+    status: 'failed',
+    },
+
+])
 const flipValue = ref(false)
 const status = ref(false)
 
@@ -22,16 +44,34 @@ const cardStatus = () => {
 </script>
 
 <template>
-    <div @click="handleFlipCard()" class="card">
-        <div class="edgingCard">
-            <span class="numberCard">01</span>
-            <p>{{ card.word }}</p>
-            <span class="eventText">Перевернуть</span>
+    <div class="wrap">
+        <div v-for="card in cards" @click="handleFlipCard()" class="card">
+            <div class="edgingCard">
+                <span class="numberCard">01</span>
+                <button v-if="card.state === 'open' && card.status === 'succes'" class="btn topText"><CheckMark/></button>
+                <button v-if="card.state === 'open' && card.status === 'failed'" class="btn topText"><Cross/></button>
+                <p v-if="card.state === 'closed'">{{ card.word }}</p>
+                <p v-if="card.state === 'open'">{{ card.translation }}</p>
+                <span v-if="card.state === 'closed'" class="eventText">Перевернуть</span>
+                <span v-if="card.state === 'open' && card.status === 'succes'" class="eventText">Завершено</span>
+                <span v-if="card.state === 'open' && card.status === 'failed'" class="eventText">Завершено</span>
+                <div v-if="card.state === 'open' && card.status === 'pending'" class="btns eventText" style="bottom: -16px;"
+                >
+                    <button class="btn"><CrossMini/></button>
+                    <button class="btn"><CheckMarkMini/></button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.wrap {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    gap: 20px;
+}
 .card {
     width: 250px;
     height: 376px;
@@ -72,5 +112,28 @@ const cardStatus = () => {
     font-size: 12px;
     background-color: var(--color-primary);
     padding: 0 4px;
+}
+
+.topText {
+    position: absolute;
+    top: -18px;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 12px;
+    z-index: 10;
+    padding: 0 4px;
+}
+
+.btns {
+    width: 80px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.btn {
+    cursor: pointer;
+    outline: none;
+    border: none;
+    background-color: transparent;
 }
 </style>
